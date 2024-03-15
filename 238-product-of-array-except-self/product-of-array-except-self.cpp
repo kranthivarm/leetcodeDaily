09252c68937c2a092c8dde5f0862cd441245fa4f
kranthivarm <119841129+kranthivarm@users.noreply.github.com>
2024-03-15 19:27:17 +0530

    Time: 13 ms (76.05%) | Memory: 26.2 MB (90.93%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int zeroCnt=0,pro=1;
+        int zeroCnt=0,pro=1,oneZeroInd=-1;
         for(int i=0;i<nums.size();i++){
-            if(!nums[i])zeroCnt++;
+            if(!nums[i]){
+                zeroCnt++;
+                oneZeroInd=i;
+            }
             else pro*=nums[i];
         }
         if(zeroCnt){
             vector<int>res(nums.size(),0);
             if(zeroCnt==1) {
-                for(int i=0;i<nums.size();i++){
-                    if(!nums[i])res[i]=pro;
-                }
+                // for(int i=0;i<nums.size();i++){
+                //     if(!nums[i])res[i]=pro;
+                // }
+                res[oneZeroInd]=pro;
             }            
             return res;
         }        

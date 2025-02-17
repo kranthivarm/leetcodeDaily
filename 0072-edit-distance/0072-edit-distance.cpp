650d8a40e0e3a3d662a3a7e2066ab9ce3042478b
kranthivarm <119841129+kranthivarm@users.noreply.github.com>
2025-02-17 22:07:34 +0530

    Time: 26 ms (19.76%), Space: 12.5 MB (83.08%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -2,6 +2,38 @@ class Solution {
 public:
     int minDistance(string w1, string w2) {
         int n=w1.size(),m=w2.size();
+        vector<int>next(m+1);
+        vector<int>curr(m+1);
+        for(int i=0;i<m;i++)next[i]=m-i;
+        for(int i=n-1;i>-1;i--){
+            // vector<int>curr(m+1);            
+            curr[m]=n-i;
+            cout<<curr[m]<<" ";
+            for(int j=m-1;j>-1;j--){
+                if(w1[i]==w2[j]){
+                    curr[j]=next[j+1];
+                    continue;
+                }
+                int x=1+curr[j+1];//insert i
+                int y=1+next[j];//insert j
+                int replace=1+next[j+1];//replace i or j
+                curr[j]=min(x,min(y,replace));
+            }
+            next=curr;
+            vector<int>t(m+1);
+            curr=t;
+        }
+        return next[0];
+    }
+};
+
+
+
+/*
+class Solution {
+public:
+    int minDistance(string w1, string w2) {
+        int n=w1.size(),m=w2.size();
         vector<vector<int>>dp(n+1,vector<int>(m+1));
         for(int i=0;i<n;i++)dp[i][m]=n-i;
         for(int i=0;i<m;i++)dp[n][i]=m-i;
@@ -19,3 +51,4 @@ public:
         return dp[0][0];
     }
 };
+*/
